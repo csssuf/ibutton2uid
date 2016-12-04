32bfae0f0a5ee45441ddf123a80703873ec4e426
James Forcier <csssuf@csssuf.net>
2016-12-03 22:10:43 -0500

    Initial code commit

---
+++ ibutton2uid.py
@@ -0,0 +1,20 @@
+from flask import Flask, abort
+from ldap.ldapobject import ReconnectLDAPObject
+import ldap
+from os import getenv
+
+app = Flask(__name__)
+ldap_conn = ReconnectLDAPObject(getenv("IBUTTON2UID_LDAP_SERVER", ""))
+ldap_conn.simple_bind_s(getenv("IBUTTON2UID_LDAP_BINDDN", ""),
+        getenv("IBUTTON2UID_LDAP_BINDPW"))
+
+@app.route("/<ibutton>/")
+def search_ibuttons(ibutton):
+    ldap_results = ldap_conn.search_s(getenv("IBUTTON2UID_LDAP_BASEOU", ""),
+            ldap.SCOPE_SUBTREE, "(ibutton=*%s)" % ibutton)
+    if len(ldap_results) != 1:
+        abort(404)
+    return ldap_results[0][1]['uid'][0].decode('utf-8')
+
+if __name__ == "__main__":
+    app.run()
